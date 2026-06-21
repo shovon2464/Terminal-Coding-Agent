a00cbc31fe735300c7c40c37a83b589e90a82d2e
shovon2464 <shovon2464@gmail.com>
2026-06-20 21:36:38 -0600

    Scaffolded header component

--- packages/cli/src/index.tsx
+++ packages/cli/src/index.tsx
@@ -1,13 +1,17 @@
-import { createCliRenderer, TextAttributes } from "@opentui/core";
+import { createCliRenderer} from "@opentui/core";
 import { createRoot } from "@opentui/react";
+import { Header } from "./components/header";
 
 function App() {
   return (
-    <box alignItems="center" justifyContent="center" flexGrow={1}>
-      <box justifyContent="center" alignItems="flex-end">
-        <ascii-font font="tiny" text="OpenTUI" />
-        <text attributes={TextAttributes.DIM}>What will you build?</text>
-      </box>
+    <box
+      alignItems="center"
+      justifyContent="center"
+      backgroundColor="#0D0D12"
+      width="100%"
+      height="100%"
+      gap={2}>
+          <Header />
     </box>
   );
 }
