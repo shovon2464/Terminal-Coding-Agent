f9dd6dff8052a672afb10edba9f47ab8a330a6c5
shovon2464 <shovon2464@gmail.com>
2026-07-22 18:14:00 -0600

    Created user-message component

---
+++ packages/cli/src/components/messages/user-message.tsx
@@ -0,0 +1,38 @@
+import { EmptyBorder } from "../border";
+import { useTheme } from "../../providers/theme";
+
+
+type Props = {
+    message: string;
+};
+
+
+export function UserMessage({ message }: Props) {
+    const { colors } = useTheme();
+
+    return (
+        <box width="100%" alignItems="center">
+            <box
+                border={["left"]}
+                borderColor={colors.primary}
+                width="100%"
+                customBorderChars={ {
+                    ...EmptyBorder,
+                    vertical: "┃",
+                    bottomT: "╹",
+                }}
+            >
+                <box
+                    justifyContent="center"
+                    paddingX={2}
+                    paddingY={1}
+                    backgroundColor={colors.surface}
+                    width="100%"
+                >
+                    <text>{message}</text>
+                </box>
+            </box>    
+
+        </box>
+    );
+};
